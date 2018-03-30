e1b965bff16263a4b77ff9e700c468b368ae51b4
winxxin <wangyiqianyi@163.com>
2018-03-30 15:03:40 +0800

    添加离线授权

---
+++ src/main/java/com/dream/brick/equipment/action/OfflineAction.java
@@ -0,0 +1,17 @@
+package com.dream.brick.equipment.action;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author 陶乐乐(wangyiqianyi@qq.com)
+ * @ClassName: OfflineAction.java
+ * @Description:
+ * @date 2018-03-30 上午10:09
+ */
+@Controller
+@Scope("prototype")
+@RequestMapping("/offline")
+public class OfflineAction {
+}
